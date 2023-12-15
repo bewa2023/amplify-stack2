9fb2ffcf9daa2fb41add08669aef0bedb7266a37
Segun Bada <bewaglobal@gmail.com>
2023-12-14 21:47:41 -0500

    app update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+Amplify.configure(config);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
